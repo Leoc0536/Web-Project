e8b44ee1f69a0887a106b76000dd7a7fc34b7854
Leoc0536 <113024598+Leoc0536@users.noreply.github.com>
2023-05-10 17:31:41 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function openMain() {
+    var x = document.getElementById("hidden");
+    x.style.display = "hidden";
+
+}
+
+
+console.log("loading");
+function paint(color) {
+    console.log(color);
+    var x = document.getElementById("circleId");
+    // x.style.backgroundColor = color;
+    x.style = `background-color:${color}`;
+    console.log(x);
+}
+
+function random(){
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+
+    let y = "rgb("+r+","+g+","+b+")";
+    let x = document.getElementById("circleId");
+    x.style.backgroundColor = y;
+    console.log(document.getElementById("colorRGB").innerHTML=`${r}, ${g}, ${b}`);
+}
